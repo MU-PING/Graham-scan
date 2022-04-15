4627f4399c300aa534addcdacdaf33950ced4d63
MU-PING <93152909+MU-PING@users.noreply.github.com>
2022-04-15 10:51:51 +0800

    Design GUI

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ Created on Tue Nov 24 22:02:29 2020
 """
 
 import matplotlib.pyplot as plt
-import tkinter as tk
 import numpy as np
+import tkinter as tk
+
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib import animation
 
@@ -18,14 +19,15 @@ class Graham_scan():
         
     def gen_data(self):
         plt.clf()
-        plt.title("Data")
+        plt.title("Data Distribution", fontsize=28)
+        plt.xlabel('x asix', fontsize=20)
+        plt.ylabel('y asix', fontsize=20)
         plt.xlim(-1200, 1200)
         plt.ylim(-1200, 1200)
         
-        data=[]
         for _ in range(points_num.get()): #群數
-            center_x = np.random.randint(-700, 700)
-            center_y = np.random.randint(-700, 700)
+            center_x = np.random.randint(-600, 600)
+            center_y = np.random.randint(-600, 600)
             self.points.append([center_x, center_y])
             plt.plot(center_x, center_y, 'o', ms=5 , color = 'gray', alpha=1) #畫圖 ms：折點大小
            
@@ -44,24 +46,24 @@ class Graham_scan():
         pass
 
 window = tk.Tk()
-window.geometry("600x650")
+window.geometry("750x650")
 window.resizable(False, False)
 window.title("Graham-scan Algorithm ")
+window.configure(bg='#E6E6FA')
 
 # Global var
 points_num = tk.IntVar()#群
 points_num.set(15)
-color = ["#FF0000", "#0000E3", "#FFD306", "#F75000", "#02DF82", "#6F00D2", "#73BF00"]
 
 # tk Frame
-setting1 = tk.Frame(window)
+setting1 = tk.Frame(window, bg="#F0FFF0")
 setting1.pack(side='top', pady=10)
 setting2 = tk.Frame(window)
 setting2.pack(side='top')
 
 # Plot
 fig = plt.figure(figsize=(8,8))
-plt.title("Data Distribution", fontsize=20)
+
 canvas = FigureCanvasTkAgg(fig, setting2)  # A tk.DrawingArea.
 canvas.get_tk_widget().grid()
 
@@ -70,12 +72,11 @@ brain = Graham_scan()
 brain.gen_data()
 
 # GUI
-tk.Label(setting1, font=("Calibri", 12, "bold"), text="Points:").grid(row=0, column=0, padx=5)
-tk.Entry(setting1, width=5, textvariable=points_num).grid(row=0, column=1)
-btn = tk.Button(setting1, font=("Calibri", 10, "bold"), text='Generate', command = brain.gen_data)
-btn.grid(row=0, column=2, padx=20)
-btn = tk.Button(setting1, font=("Calibri", 10, "bold"), text='Start', command = brain.start)
-btn.grid(row=0, column=3)
+tk.Label(setting1, font=("Calibri", 15, "bold"), text="Number of points:", bg="#F0FFF0").pack(side='left', padx=5)
+tk.Entry(setting1, width=5, textvariable=points_num).pack(side='left')
+btn = tk.Button(setting1, font=("Calibri", 12, "bold"), text='Generate Points', command = brain.gen_data)
+btn.pack(side='left', padx=(10, 5), pady=5)
+btn = tk.Button(setting1, font=("Calibri", 12, "bold"), text='Start finding convex hull', command = brain.start)
+btn.pack(side='left', padx=(5, 10), pady=5)
 
 window.mainloop()
-
